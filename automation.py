1b5ccd5308b4500b73f4d1cc2e877a3587e06181
Max Sampson <msampson113@gmail.com>
2021-08-05 15:47:23 -0500

    minor comments added

--- automation.py
+++ automation.py
@@ -6,14 +6,14 @@ import os
 ## file path
 ## tensorboard run directory
 ## Number of max epochs (test with 1 to make sure it works before doing anything larger)
-
+## file_name() is unnecessary, just name the files train.laz and validation.laz respectively
 
 def file_location():
     user_location = input("Enter the location of the training data (directory/folder, not file): \n")
     assert os.path.exists(user_location), "I did not find the location at, " + str(user_location)
     return str(user_location)
 
-def file_name():
+def file_name(): #this might be unnecessary
     user_name = input("Enter the name of the training data file (file only, not directory): \n")
     assert os.path.exists(user_name), "I did not find the file named " + str(user_name)
     return str(user_name)
